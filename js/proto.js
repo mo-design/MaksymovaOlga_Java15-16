b89620d63c200a428efb51f06470f315dca74509
Olga Maksymova <maximova.olga@gmail.com>
2016-05-16 14:07:06 +0300

    files were added

---
+++ js/proto.js
@@ -0,0 +1,92 @@
+﻿$(document).ready(function () {
+	
+	
+	
+	function Human(name, age) {
+		this.name = name;		
+		this.age = age;
+		this.gen = 'M';
+		this.height = 180;
+		this.weight = 80;
+	};
+	
+	
+	
+	
+	function Worker(work, salary)  {
+		this.workPlace = work;
+		this.salary = salary;
+		this.work = function() {
+					console.log('The place of my work is '+ this.workPlace);
+					},
+		this.__proto__ =  Human
+	}
+
+
+	function Student(vuz, serName) {
+		
+		this.studyPlace = vuz;
+		this.salary = 50;
+		this.seriesTV = function() {
+						console.log('Я смотрю сериал "' + serName + '"');
+						};	
+	}
+	
+	
+	Student.prototype = Human;
+	
+	
+	
+	
+	var man1 = new Human ('Иван', 35);
+	
+	var worker1 = new Worker('Завод ХТЗ', 250);
+	
+	worker1.__proto__ = man1;
+
+	
+	console.log('Имя - ' + worker1.name + '.');
+	console.log('Возраст - ' + worker1.age + '.');
+	console.log('Среднестатистический вес - ' + worker1.weight + '.');
+	console.log('Место работы - ' + worker1.workPlace + '.');
+	
+	console.log('-------------------');
+	
+	
+	var student = new Student('ХПИ', 'Игры престолов');
+	
+	
+	console.log('Мое имя - ' + student.name + '. \n/потому что еще не задано/.');
+	console.log('Я учусь в ' + student.studyPlace + '.');
+	student.seriesTV();
+	
+	console.log('-------------------');
+
+	
+		
+	
+	var man2 = new Human ('Дима', 18);
+	student.__proto__ = man2;
+	
+	console.log('Мое имя - ' + student.name + '. \n/уже задано/.');
+	console.log('Мой возраст - ' + student.age + ' лет.');
+	console.log('Я учусь в ' + student.studyPlace + '.');
+	student.seriesTV();
+	
+	console.log('-------------------');
+	
+	
+	var man3 = new Human ('Игорь', 20);
+	var student2 = new Student('ХНМУ', 'Доктор Хаус')
+	
+	student2.__proto__ = man3;
+	
+	console.log('Мое имя - ' + student2.name + '.');
+	console.log('Мой возраст - ' + student2.age + ' лет.');
+	console.log('Я учусь в ' + student2.studyPlace + '.');
+	student2.seriesTV();
+	
+	console.log('-------------------');
+	
+	
+});
---
+++ js/script.js
@@ -0,0 +1,87 @@
+﻿$(document).ready(function () {
+	
+	
+	
+	$('input').keydown(function(event){
+		
+		if(event.keyCode === 13){ apiStart();}
+    });
+	
+	
+	
+	
+	$('#sendButton').click(apiStart);
+
+
+	
+	function apiStart() {
+	
+	
+		var zaprosText = $('#zapros').val();
+	
+
+			$.ajax({
+          
+			url: 'http://api.riffsy.com/v1/search',
+			data: {
+				limit: 24,
+				tag: zaprosText,
+			},
+	
+			dataType : "json",
+			success: function (data) {
+						console.log(data);
+					
+					
+					
+						if (data.results.length == 0){
+							
+							var inner = '<p>По запросу <b>' + zaprosText + '</b> ничего не найдено.</p><p>Рекомендации:</p><ul><li> Убедитесь, что все слова написаны без ошибок.</li><li>Попробуйте использовать другие ключевые слова.</li><li>Попробуйте использовать более популярные ключевые слова.</li><li>Попробуйте уменьшить количество слов в запросе.</li></ul>';
+							$('.answers').html(inner);	
+					
+						}
+						
+						
+						else {
+							
+							
+							var ul = document.createElement("ul");
+							
+							$.each(data.results, function(i, val){
+							
+								var li = document.createElement("li");
+					
+								var inner = '<img src="' + val.url + '" class="img">' + '<a href="'+val.url+'" class="imgA" target="_blank">'+val.title+"</a>";
+							
+								li.innerHTML = inner;
+								li.className='newli';
+								ul.appendChild(li);
+								
+								
+							});
+						
+							$('.answers').html(ul);
+
+						};
+						
+						
+						
+					}
+			
+	
+
+	});	
+	
+
+	
+	
+}
+	
+	
+
+	
+	
+
+	
+	
+});
